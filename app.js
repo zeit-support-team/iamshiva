71052b0b95ef691257d14783a9e56e26298a2d90
shivajangra <shivajangra303@gmail.com>
2020-01-17 23:45:01 +0530

    file

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
-'use strict'; 
+'use strict';
 // define globals
 var express = require('express'),
     path = require('path'),
     app = express();
-    
-app.use(express.static(__dirname +  '/'));
+
+app.use(express.static(__dirname +  '/index.html'));
 app.get('*', function (req, res){
-    res.sendFile(path.join(__dirname + '/'));
+    res.sendFile(path.join(__dirname + '/index.html'));
 });
 app.listen(8080);
-  
